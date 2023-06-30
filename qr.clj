9f57788dc7020c5af669b53ea6c6b808a161c77c
ell1jah <ilya.grinkewitch@yandex.ru>
2023-06-30 12:30:32 +0300

    Formatted clj

--- qr.clj
+++ qr.clj
@@ -42,32 +42,69 @@
   (reduce + (map * arr1 arr2)))
 
 (defn get-projection [arr1 arr2]
-  (map #(* % (/ (get-dot-product arr1 arr2) (* (get-norm arr2) (get-norm arr2)))) arr2))
+  (map 
+   #(* % (/
+          (get-dot-product arr1 arr2)
+          (* (get-norm arr2) (get-norm arr2))))
+   arr2))
 
 (defn q-elem [a i j]
-  (/ (nth (reduce (fn [x y] (map - x y)) (concat [(get-column a j)] (map (fn [x] (get-projection (get-column a j) x)) (q-cols a j)))) i) (r-elem a j j)))
+  (/ 
+   (nth 
+    (reduce 
+     (fn [x y] (map - x y))
+     (concat
+      [(get-column a j)]
+      (map
+       (fn [x] (get-projection (get-column a j) x))
+       (q-cols a j))))
+    i) 
+   (r-elem a j j)))
 
 (defn r-elem [a i j]
   (cond
-    (= i j) (get-norm (reduce (fn [x y] (map - x y)) (concat [(get-column a j)] (map (fn [x] (get-projection (get-column a j) x)) (q-cols a j)))))
-    (< i j) (get-dot-product (get-column a j) (map (fn [y] (q-elem a (dec y) i)) (create-list (count a))))
+    (= i j) (get-norm
+             (reduce
+              (fn [x y] (map - x y))
+              (concat
+               [(get-column a j)]
+               (map
+                (fn [x] (get-projection (get-column a j) x))
+                (q-cols a j)))))
+    (< i j) (get-dot-product
+             (get-column a j)
+             (map
+              (fn [y] (q-elem a (dec y) i)) 
+              (create-list (count a))))
     (> i j) 0))
 
 (defn q-cols [a j]
-  (map (fn [x] (map (fn [y] (q-elem a (dec y) (dec x))) (create-list (count a)))) (create-list j)))
+  (map
+   (fn [x] (map
+            (fn [y] (q-elem a (dec y) (dec x)))
+            (create-list (count a))))
+   (create-list j)))
 
 (defn qr-j-rec [a elem i j]
   (cond
     (< j 0) nil
-    (>= j 0) (concat (qr-j-rec a elem i (dec j)) (list (elem a i j)))))
+    (>= j 0) (concat
+              (qr-j-rec a elem i (dec j))
+              (list (elem a i j)))))
 
 (defn qr-i-rec [a elem i]
   (cond
-    (= i 0) (list (qr-j-rec a elem i (dec (count (first a)))))
-    (> i 0) (concat (qr-i-rec a elem (dec i)) (list (qr-j-rec a elem i (dec (count (first a))))))))
+    (= i 0) (list
+             (qr-j-rec a elem i (dec (count (first a)))))
+    (> i 0) (concat
+             (qr-i-rec a elem (dec i))
+             (list
+              (qr-j-rec a elem i (dec (count (first a))))))))
 
 (defn qr [a]
-  (list (qr-i-rec a q-elem (dec (count a))) (qr-i-rec a r-elem (dec (count (first a))))))
+  (list
+   (qr-i-rec a q-elem (dec (count a)))
+   (qr-i-rec a r-elem (dec (count (first a))))))
 
 (println (qr [[10 -3 1] [4 5 2] [1 2 7] [4 2 75]]))
 
